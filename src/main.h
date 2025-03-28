9f40d9bb471bc81d295f6c87b5a926669ad95d57
Anirudh Konidala <kidskodingclub@gmail.com>
2025-03-27 20:11:24 -0500

    added two_sum and reverse_vowels

---
+++ src/main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target);
+
+std::string reverseVowels(std::string s);
+bool isVowel(char c);
---
+++ src/two_sum.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <map>
+#include <utility>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+	std::map<int, int> map;
+	for(int i = 0; i < nums.size(); i++) {
+		int difference = target - nums[i];
+		if(map.find(difference) != map.end()) {
+			return {map[difference], i};
+		} else {
+			map.insert(std::make_pair(nums[i], i));
+		}
+	}
+	return std::vector<int>();
+}
